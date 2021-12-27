67db75800c71a311d449b398de824ea2477b4d48
JoeyYoung <Cookie_8421@sina.cn>
2021-12-27 15:27:43 +0800

    FileDownloadMethod

--- spiderman/src/main/java/com/example/spiderman/utils/FileTool.java
+++ spiderman/src/main/java/com/example/spiderman/utils/FileTool.java
@@ -8,15 +8,18 @@ package com.example.spiderman.utils;
  */
 import com.example.spiderman.page.Page;
 
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Vector;
 
 /*  本类主要是 下载那些已经访问过的文件*/
 public class FileTool {
 
     private static String dirPath;
+    private static int BUFFER_SIZE = 10240; // 缓冲区大小(缓冲区越大下载的越快,但是要根据自己的服务器配置)
+    private Vector vDownLoad = new Vector(); // URL列表
+    private Vector vFileList = new Vector(); // 下载后的保存文件名列表
 
 
     /**
@@ -72,6 +75,87 @@ public class FileTool {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 清除下载列表
+     */
+    public void resetList() {
+        vDownLoad.clear();
+        vFileList.clear();
+    }
+
+    /**
+     * 增加下载列表项
+     *
+     * @param url
+     *            String
+     * @param filename
+     *            String
+     */
+
+    public void addItem(String url, String filename) {
+        vDownLoad.add(url);
+        vFileList.add(filename);
+    }
+
+    /**
+     * 根据列表下载资源
+     */
+    public void downLoadByList() {
+        String url = null;
+        String filename = null;
+
+        // 按列表顺序保存资源
+        for (int i = 0; i < vDownLoad.size(); i++) {
+            url = (String) vDownLoad.get(i);
+            filename = (String) vFileList.get(i);
+
+            try {
+                saveToFile(url, filename);
+            } catch (IOException err) {
+                System.out.println("资源[" + url + "]下载失败!!!");
+            }
+        }
+        System.out.println("下载完成!!!");
+    }
+
+    /**
+     * 将HTTP资源另存为文件
+     *
+     * @param destUrl
+     *            String
+     * @param fileName
+     *            String
+     * @throws Exception
+     */
+    public void saveToFile(String destUrl, String fileName) throws IOException {
+        FileOutputStream fos = null;
+        BufferedInputStream bis = null;
+        HttpURLConnection httpUrl = null;
+        URL url = null;
+        byte[] buf = new byte[BUFFER_SIZE];
+        int size = 0;
+
+        // 建立链接
+        url = new URL(destUrl);
+        httpUrl = (HttpURLConnection) url.openConnection();
+        // 连接指定的资源
+        httpUrl.connect();
+        // 获取网络输入流
+        bis = new BufferedInputStream(httpUrl.getInputStream());
+        // 建立文件
+        fos = new FileOutputStream(fileName);
+
+        System.out.println("正在获取链接[" + destUrl + "]的内容...\n将其保存为文件["
+                    + fileName + "]");
+        // 保存文件
+        while ((size = bis.read(buf)) != -1)
+            fos.write(buf, 0, size);
+
+        fos.close();
+        bis.close();
+        httpUrl.disconnect();
     }
 
 }
